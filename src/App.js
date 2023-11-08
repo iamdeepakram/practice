a3439974f578909de3d411388ac7bf1d7fe36d29
iamdeepakram <deepakramsutrakar@gmail.com>
2023-11-08 09:52:17 +0530

    latest update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-import EmailSubscription from './components/email/EmailSubscription';
 import girImg from './components/img/girlImg.png'
+
 
 function App() {
   return (
     <div className="App">
-      <div className='container' >
-      <div className='container-item' >
-          <h1>BASE APPAREL</h1>
+      <div className='container'>
+        <div className='container-item item1 ' >
+         <span className='circle' ></span> <p className='header' >BASE APPAREL</p>
         </div>
-        <div className='container-item' >
+        <div className='container-item item2 ' >
           <img src={girImg}  className='image' ></img>
+
         </div>
-        <div className='container-item' >
-          <EmailSubscription/>
+        <div className='container-item item3 ' >
+        <section className="email">
+            <div>
+              <h1>WE'RE</h1>
+            </div>
+            <div>
+            <h2>COMING</h2>
+            </div>
+            <div>
+              <h2>SOON</h2>
+            </div>
+            <div>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem psum has been the industry’s standard dummy Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            </div>
+              <form>
+                <input type="email" placeholder="Email address" />
+                <button>></button>
+              </form>
+          </section>
         </div>
       </div>
     </div>
